4b01fc4b767d301c1219301e24b088471aeb02e9
Skyline-23 <bs2740@naver.com>
2021-07-23 22:46:09 +0900

    [UPDATE] Acknowlist view name

--- snapkit-preview-test/Preview.swift
+++ snapkit-preview-test/Preview.swift
@@ -8,22 +8,22 @@
 #if canImport(SwiftUI) && DEBUG
 import SwiftUI
 
-struct ViewControllerPreview: PreviewProvider {
-    static var previews: some View {
-        UIViewControllerPreview {
-            let controller = ViewController()
-            return controller
-        }
-    }
-}
-
-//struct ViewPreview: PreviewProvider {
+//struct ViewControllerPreview: PreviewProvider {
 //    static var previews: some View {
-//        UIViewPreview {
-//            let textField = customTextField()
-//            return textField
-//        }.previewLayout(.sizeThatFits)
+//        UIViewControllerPreview {
+//            let controller = ViewController()
+//            return controller
+//        }
 //    }
 //}
+
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        UIViewPreview {
+            let textField = customTextField()
+            return textField
+        }.previewLayout(.sizeThatFits)
+    }
+}
 
 #endif
--- snapkit-preview-test/Source/ViewController/ViewController.swift
+++ snapkit-preview-test/Source/ViewController/ViewController.swift
@@ -16,14 +16,16 @@ class ViewController: UIViewController {
     }
     
     let button = UIButton().then {
-        $0.backgroundColor = .blue
-        $0.setTitle("hello world", for: .normal)
+        $0.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        $0.layer.cornerRadius = 10
+        $0.setTitle("오픈소스 보기", for: .normal)
         $0.addTarget(self, action: #selector(touched(_:)), for: .touchUpInside)
     }
     
     //MARK: - Action
     @objc func touched(_ sender: UIButton) {
         let viewcontroller = AcknowListViewController()
+        viewcontroller.title = "오픈소스 라이브러리"
         let navigationcontroller = UINavigationController(rootViewController: viewcontroller)
         present(navigationcontroller, animated: true, completion: nil)
     }
@@ -36,10 +38,11 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         self.button.snp.makeConstraints {
-            $0.left.equalToSuperview().offset(32)
-            $0.right.equalToSuperview().offset(-32)
-            $0.top.equalToSuperview().offset(32)
-            $0.height.equalTo(90)
+            $0.left.equalTo(self.view).offset(32)
+            $0.right.equalTo(self.view).offset(-32)
+            $0.height.equalTo(45)
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-32)
+//            $0.center.equalTo(self.view).offset(50)
         }
         // Do any additional setup after loading the view.
     }
